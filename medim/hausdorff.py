4130af94b86c32d06fb8a3807d060cce0f072741
Max <me@example.com>
2017-06-10 13:59:18 +0300

    added weights to hausdorff distance

--- medim/hausdorff.py
+++ medim/hausdorff.py
@@ -1,4 +1,4 @@
-from hausdorff import hausdorff as _hausdorff
+from hausdorff import weighted_hausdorff
 
 
 # Before using this module, install the dependecies:
@@ -9,7 +9,7 @@ from hausdorff import hausdorff as _hausdorff
 # python setup.py build && python setup.py install
 
 
-def hausdorff(a, b, label=1):
+def hausdorff(a, b, weights=1, label=1):
     """
     Calculates the Hausdorff distance between two masks.
     
@@ -17,11 +17,21 @@ def hausdorff(a, b, label=1):
     ----------
     
     a, b: ndarray
-       The arrays containing the masks
+        The arrays containing the masks. Their ndim must match.
     label: int, default = 1
-       The label of the mask
+        The label of the mask
+    weights: number or array/list/tuple
+        The weight along each axis (for anisotropic grids). If array, its length must 
+        match the ndim of the array. If number, all the axes will have the same weight
     """
+    
+    try:
+#         check if array
+        len(weights)
+    except TypeError:
+        weights = [weights] * a.ndim 
+    weights = np.array(weights, 'float64')
     
     def prep(x):
         return np.argwhere(x == label).copy(order='C').astype('float64')
-    return _hausdorff(prep(a), prep(b))
+    return weighted_hausdorff(prep(a), prep(b), weights)
