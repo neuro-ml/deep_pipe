4903433a0544fbb6ea1dc5d6779ad1829b89f423
Egor Krivov <egor.krivov@frtk.ru>
2017-06-10 18:41:10 +0300

    new patch iterator

--- medim/batch_iter/patch.py
+++ medim/batch_iter/patch.py
@@ -1,5 +1,4 @@
-from itertools import product
-from typing import *
+import random
 
 import numpy as np
 
@@ -41,7 +40,7 @@ def extract_batch_patches(x: np.ndarray, x_out: np.ndarray, center_idx,
 
 
 def uniform(xs: list, patch_sizes: list, *, batch_size: int,
-            spatial_dims: list):
+            spatial_dims: 2):
     """Patch iterator with uniformed distribution over spatial dimensions.
     First we choose patch for the last patch size and then for the rest."""
     patch_sizes = np.array(patch_sizes)
@@ -69,6 +68,64 @@ def uniform(xs: list, patch_sizes: list, *, batch_size: int,
         start_idx = np.random.rand(batch_size, 3) * max_spatial_idx[idx]
         start_idx = np.int32(np.floor(start_idx))
         center_idx = start_idx + patch_sizes[-1] // 2
+
+        for d in range(n_sources):
+            extract_batch_patches(extract(xs[d], idx), batches[d], center_idx,
+                                  spatial_dims, patch_sizes[d])
+
+        yield [np.array(b) for b in batches]
+
+
+def foreground(xs: list, patch_sizes: list, *, batch_size: int, spatial_dims,
+               f_fraction: float, f_condition: callable):
+    """Patch iterator with uniformed distribution over spatial dimensions for 
+    1-f_fraction part of the batch and uniformed distribution over patches with
+    f_condition==true in the center for f_fraction part of the batch.
+    First we choose patch for the last patch size and then for the rest."""
+    patch_sizes = np.array(patch_sizes)
+    assert np.all((patch_sizes % 2)[0:1] == (patch_sizes % 2))
+
+    n_sources = len(xs)
+    n_objects = len(xs[0])
+
+    f_objects = int(batch_size * f_fraction)
+
+    spatial_dims = np.array(spatial_dims)
+    spatial_shapes = get_spatial_sizes(xs, spatial_dims)
+
+    # Get max spatial starting index for the last data
+    max_spatial_idx = spatial_shapes - patch_sizes[-1] + 1
+
+    # Get conditional indices
+    conditional_indices = []
+    for i in range(n_objects):
+        c = np.argwhere(f_condition(xs[-1][i]))
+
+        l_bound = c - patch_sizes[-1] // 2
+        r_bound = c + patch_sizes[-1] // 2 + patch_sizes[-1]%2
+
+        # Remove centers that are too left
+        c = c[np.all(l_bound >= 0, axis=1)]
+        # Remove centers that are too right
+        c = c[np.all(r_bound <= spatial_shapes[i], axis=1)]
+        conditional_indices.append(c)
+
+    # Create batches variables with the right shape and type
+    batches = []
+    for i in range(n_sources):
+        shape = np.array(xs[i][0].shape)
+        shape[spatial_dims] = patch_sizes[i]
+        batches.append(np.zeros((batch_size, *shape), dtype=xs[i][0].dtype))
+
+    while True:
+        # get center index
+        idx = np.random.randint(n_objects, size=batch_size)
+        start_idx = np.random.rand(batch_size, 3) * max_spatial_idx[idx]
+        start_idx = np.int32(np.floor(start_idx))
+        center_idx = start_idx + patch_sizes[-1] // 2
+
+        for i in range(f_objects):
+            center_idx[i] = random.choice(conditional_indices[idx[i]])
 
         for d in range(n_sources):
             extract_batch_patches(extract(xs[d], idx), batches[d], center_idx,
