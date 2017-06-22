151c403a07e4c301f172fcfa6567ae5c6c1c986d
Max <me@example.com>
2017-06-23 01:08:56 +0300

    added isles

--- experiments/experiments/datasets/config.py
+++ experiments/experiments/datasets/config.py
@@ -1,15 +1,27 @@
 from .base import Dataset
 from .brats import Brats2015, Brats2017
-
+from .isles import siss_factory, spes_factory
 
 dataset_name2dataset = {
     'brats2015': Brats2015,
     'brats2017': Brats2017,
+
+    'isles_siss': siss_factory('siss2015.csv'),
+    'isles_siss_augmented': siss_factory('augmented_siss.csv'),
+    'isles_spes': spes_factory('spes2015.csv'),
+    'isles_spes_augmented_core': spes_factory('augmented_spes_core.csv'),
+    'isles_spes_augmented_penumbra': spes_factory('augmented_spes_penumbra.csv'),
 }
 
 dataset_name2default_path = {
     'brats2015': '/home/mount/neuro-x02-ssd/brats2015/processed',
     'brats2017': '/home/mount/neuro-x02-ssd/brats2017/processed',
+
+    'isles_siss': '/home/mount/neuro-x04-hdd/ISLES/',
+    'isles_siss_augmented': '/home/mount/neuro-x04-hdd/ISLES/',
+    'isles_spes': '/home/mount/neuro-x04-hdd/ISLES/',
+    'isles_spes_augmented_core': '/home/mount/neuro-x04-hdd/ISLES/',
+    'isles_spes_augmented_penumbra': '/home/mount/neuro-x04-hdd/ISLES/',
 }
 
 
---
+++ experiments/experiments/datasets/isles.py
@@ -0,0 +1,109 @@
+import pandas as pd
+import numpy as np
+from os.path import join
+import nibabel as nib
+from scipy.ndimage import zoom
+
+from .base import Dataset
+
+
+class Isles(Dataset):
+    def __init__(self, processed_path):
+        super().__init__(processed_path)
+        self.processed_path = processed_path
+        self.metadata = pd.read_csv(join(processed_path, self.filename))
+        self._patient_ids = self.metadata.index.values
+
+    def load_mscan(self, patient_id):
+        channels = self.metadata.iloc[patient_id][self.modalities]
+        result = []
+
+        for row in channels:
+            res = []
+            for image in row:
+                image = image.replace('data/', self.processed_path)
+                x = nib.load(image).get_data()
+                x = self.adjust(x)
+                x = x.astype('float32')
+                m, M = x.min(), x.max()
+                x = (x - m) / (M - m)
+                res.append(x)
+
+            result.append(np.asarray(res))
+
+        return np.asarray(result)
+
+    def load_segm(self, patient_id):
+        # dunno what to do here
+        pass
+
+    def load_msegm(self, patient):
+        channels = self.metadata.iloc[patient][self.labels]
+        result = []
+
+        for row in channels:
+            res = []
+            for x in [nib.load(image).get_data() for image in row]:
+                x = self.adjust(x, True)
+                res.append(x)
+
+            result.append(np.asarray(res))
+
+        return np.asarray(result)
+
+    def segm2msegm(self, segm):
+        #  and here too
+        pass
+
+    @property
+    def patient_ids(self):
+        return list(range(len(self.metadata)))
+
+    @property
+    def n_chans_mscan(self):
+        return len(self.modalities)
+
+    @property
+    def n_chans_msegm(self):
+        return len(self.labels)
+
+    @property
+    def n_classes(self):
+        return len(self.labels)
+
+
+def siss_factory(file):
+    class IslesSISS(Isles):
+        modalities = ['T1', 'T2', 'Flair', 'DWI']
+        labels = ['OT']
+        filename = file
+
+        def adjust(self, x, label=False):
+            if x.shape[-1] != 154:
+                x = np.pad(x, ((0, 0), (0, 0), (0, 1)), mode='constant')
+            return x
+
+        @property
+        def spatial_size(self):
+            return 230, 230, 154
+
+    return IslesSISS
+
+
+def spes_factory(file):
+    class IslesSPES(Isles):
+        modalities = ['CBF', 'CBV', 'DWI', 'T1c', 'T2', 'TTP', 'Tmax']
+        labels = ['penumbralabel', 'corelabel']
+        filename = file
+
+        def adjust(self, x, label=False):
+            ref_shape = np.array(self.spatial_size)
+            order = 0 if label else 3
+            x = zoom(x, ref_shape / x.shape, order=order)
+            return x
+
+        @property
+        def spatial_size(self):
+            return 96, 110, 72
+
+    return IslesSPES
