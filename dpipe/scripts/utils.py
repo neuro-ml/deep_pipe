ac78019473e87b7e516808562ac2f1fb7154450a
Egor Krivov <egor.krivov@frtk.ru>
2017-07-12 22:28:48 +0300

    New config and changed read_lines logic

--- dpipe/scripts/utils.py
+++ dpipe/scripts/utils.py
@@ -1,4 +1,3 @@
 def read_lines(path):
-    with open(path, 'r') as f:
-        lines = f.read().split('\n')
-    return [l for l in lines if l != '']
+    with open(path, 'r') as file:
+        return [l for l in map(lambda x: x.strip(), file) if l != '']
