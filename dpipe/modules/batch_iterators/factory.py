f70bd53f7967684bb3e074dd29c18cec7b5bd5d8
Egor Krivov <egor.krivov@frtk.ru>
2017-07-13 14:33:36 +0300

    batch iterator factories now support batch iterators with context

--- dpipe/modules/batch_iterators/factory.py
+++ dpipe/modules/batch_iterators/factory.py
@@ -1,47 +1,63 @@
 from itertools import islice
-from contextlib import suppress
-from abc import abstractmethod, ABC
+from contextlib import suppress, contextmanager
+from abc import abstractmethod, ABCMeta
 
 
-class BatchIter(ABC):
+@contextmanager
+def build_contextmanager(o):
+    yield o
+
+
+def maybe_build_contextmanager(o):
+    if hasattr(o, '__enter__'):
+        return o
+    else:
+        return build_contextmanager(o)
+
+
+class BatchIterFactory(metaclass=ABCMeta):
     @abstractmethod
-    def __iter__(self):
+    def __next__(self):
         pass
+
+    def __iter__(self):
+        return self
 
     def __enter__(self):
-        pass
+        return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         pass
 
 
-class BatchIterFin(BatchIter):
+class BatchIterFactoryFin(BatchIterFactory):
     def __init__(self, get_batch_iter):
         self.get_batch_iter = get_batch_iter
 
-    def __iter__(self):
-        return iter(self.get_batch_iter())
+    def __next__(self):
+        return maybe_build_contextmanager(self.get_batch_iter())
 
 
-class BatchIterInf(BatchIter):
+class BatchIterFactoryInf(BatchIterFactory):
     def __init__(self, get_batch_iter, n_iters_per_batch):
-        self.batch_iter = get_batch_iter()
+        self.inf_batch_iter = get_batch_iter()
         self.n_iters_per_batch = n_iters_per_batch
 
-    def __iter__(self):
-        return islice(self.batch_iter, self.n_iters_per_batch)
+    def __next__(self):
+        return build_contextmanager(
+            islice(self.inf_batch_iter, self.n_iters_per_batch))
 
     def __enter__(self):
         with suppress(AttributeError):
-            self.batch_iter.__enter__()
+            self.inf_batch_iter.__enter__()
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         with suppress(AttributeError):
-            return self.batch_iter.__exit__(exc_type, exc_val, exc_tb)
+            return self.inf_batch_iter.__exit__(exc_type, exc_val, exc_tb)
 
 
-def build_batch_iter(get_batch_iter, n_iters_per_batch) -> BatchIter:
+def build_batch_iter(get_batch_iter, n_iters_per_batch) -> BatchIterFactory:
     if n_iters_per_batch is None:
-        return BatchIterFin(get_batch_iter)
+        return BatchIterFactoryFin(get_batch_iter)
     else:
-        return BatchIterInf(get_batch_iter, n_iters_per_batch)
+        return BatchIterFactoryInf(get_batch_iter, n_iters_per_batch)
