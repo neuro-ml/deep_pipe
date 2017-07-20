a016365f5237e92de2981dc2ee9b771c72f89fda
Egor Krivov <egor.krivov@frtk.ru>
2017-07-20 14:28:20 +0300

    frozen model

--- dpipe/scripts/simple/predict.py
+++ dpipe/scripts/simple/predict.py
@@ -1,34 +1,32 @@
 import os
 
 import numpy as np
+import tensorflow as tf
 
-from dpipe.config import config_dataset, config_model
+from dpipe.config import config_dataset, config_frozen_model
 from dpipe.config.default_parser import get_config
-from dpipe.modules.dl import ModelController, Optimizer
 from utils import read_lines
 
 if __name__ == '__main__':
-    config = get_config('ids_path', 'restore_model_path',
-                        'predictions_path', 'log_path')
+    config = get_config('ids_path', 'restore_model_path', 'predictions_path')
 
-    results_path = config['predictions_path']
+    predictions_path = config['predictions_path']
     ids_path = config['ids_path']
     restore_model_path = config['restore_model_path']
-    log_path = config['log_path']
     dataset = config_dataset(config)
-    model = config_model(config, optimizer=Optimizer(),
-                         n_chans_in=dataset.n_chans_mscan,
-                         n_chans_out=dataset.n_chans_msegm)
+    frozen_model = config_frozen_model(config, n_chans_in=dataset.n_chans_mscan,
+                                       n_chans_out=dataset.n_chans_msegm)
 
     ids = read_lines(ids_path)
-    os.makedirs(results_path)
+    os.makedirs(predictions_path)
 
-    with ModelController(model, log_path, restore_model_path) as mc:
+    with tf.Session(frozen_model.graph) as session:
+        frozen_model.prepare(session, restore_model_path)
         for identifier in ids:
-            x = dataset.load_mscan(identifier)
-            y = mc.predict_object(x)
+            x = dataset.load_x(identifier)
+            y = frozen_model.predict_object(x)
 
-            np.save(os.path.join(results_path, str(identifier)), y)
+            np.save(os.path.join(predictions_path, str(identifier)), y)
 
             # saving some memory
             del x, y
