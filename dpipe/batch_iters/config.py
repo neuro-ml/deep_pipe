4a01b53b093ff474ec407d6d6e633da2f2f65030
Egor Krivov <egor.krivov@frtk.ru>
2017-08-04 21:06:07 +0300

    batch iter config minor fix

--- dpipe/batch_iters/config.py
+++ dpipe/batch_iters/config.py
@@ -1,5 +1,5 @@
-from .patch_3d_stratified import make_3d_patch_stratified_iter
 from .slices import make_slices_iter, make_multiple_slices_iter
+from .patch_3d_stratified import make_3d_patch_stratified_iter
 from .patch_3d_stratified_augm import make_3d_augm_patch_stratified_iter
 
 batch_iter_inf_name2batch_iter = {
