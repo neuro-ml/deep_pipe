5e000feb3d9ab10a266cc163ff875fed5e75bb38
Egor Krivov <egor.krivov@frtk.ru>
2017-08-20 21:29:31 +0300

    binarize was updated

--- scripts/binarize.py
+++ scripts/binarize.py
@@ -2,18 +2,19 @@ import os
 
 import numpy as np
 
-from dpipe.config import get_config, ResourceManager
+from dpipe.config import get_config, get_resource_manager
 from utils import load_by_id
 
 if __name__ == '__main__':
-    config = get_config('ids_path', 'predictions_path', 'thresholds_path',
-                        'binary_predictions_path')
-    resource_manager = ResourceManager(config)
+    resource_manager = get_resource_manager(get_config(
+        'config_path', 'ids_path', 'predictions_path', 'thresholds_path',
+        'binary_predictions_path'
+    ))
 
     binary_predictions_path = resource_manager['binary_predictions_path']
     ids = resource_manager['ids']
-    thresholds_path = config['thresholds_path']
-    predict_path = config['predictions_path']
+    thresholds_path = resource_manager['thresholds_path']
+    predict_path = resource_manager['predictions_path']
 
     thresholds = np.load(thresholds_path)
     channels = len(thresholds)
