3c7c15c0d097f3540d507869d17ef30b10cd1483
Egor krivov <egor.krivov@frtk.ru>
2017-09-05 15:33:55 +0300

    brats 2017 was fixed and tested

--- dpipe/dataset/brats.py
+++ dpipe/dataset/brats.py
@@ -1,24 +1,28 @@
-import functools
-
 import numpy as np
 
 from .from_metadata import FromMetadata
 
 
-Brats2017 = functools.partial(
-    FromMetadata,
-    metadata_rpath='metadata.csv',
-    modalities=['T1', 'T1CE', 'T2', 'flair'],
-    target='segm',
-    segm2msegm_matrix=np.array(
-        [
-            [0, 0, 0],
-            [1, 1, 0],
-            [1, 0, 0],
-            [1, 1, 1]
-        ], dtype=bool
-    )
-)
+# We need this class because in the original data segm values are [0, 1, 2, 4]
+class Brats2017(FromMetadata):
+    def __init__(self, data_path):
+        super().__init__(
+            data_path=data_path,
+            metadata_rpath='metadata.csv',
+            modalities=['t1', 't1ce', 't2', 'flair'],
+            target='segm',
+            segm2msegm_matrix=np.array([
+                [0, 0, 0],
+                [1, 1, 0],
+                [1, 0, 0],
+                [1, 1, 1]
+            ], dtype=bool)
+        )
+
+    def load_segm(self, patient_id):
+        segm = super().load_segm(patient_id)
+        segm[segm == 4] = 3
+        return segm
 
 # For Brats 2015
 # segm2msegm = np.array([
