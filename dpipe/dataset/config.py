3c0676d4989e33a8a4a79a6025c260d6b92f50d1
Egor krivov <egor.krivov@frtk.ru>
2017-09-08 18:44:28 +0300

    added groups wrapper to config.py

--- dpipe/dataset/config.py
+++ dpipe/dataset/config.py
@@ -3,7 +3,7 @@ from .isles import *
 from .whitematter import WhiteMatterHyperintensity
 
 from .wrappers import make_cached, make_bbox_extraction, make_normalized, \
-    make_normalized_sub
+    make_normalized_sub, add_groups
 
 name2dataset = {
     'brats2017': Brats2017,
@@ -32,5 +32,6 @@ name2dataset_wrapper = {
     'cached': make_cached,
     'normalized': make_normalized,
     'normalized_sub': make_normalized_sub,
-    'bbox_extraction': make_bbox_extraction
+    'bbox_extraction': make_bbox_extraction,
+    'groups': add_groups,
 }
