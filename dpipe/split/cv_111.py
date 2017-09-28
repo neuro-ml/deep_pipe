d9f48bcd27fc29d6503e3444aef4dabaf535b91d
Egor krivov <egor.krivov@frtk.ru>
2017-09-28 20:48:23 +0300

    fixed bug in group splitter that would mix train, val and test sets

--- dpipe/split/cv_111.py
+++ dpipe/split/cv_111.py
@@ -1,22 +1,39 @@
 import numpy as np
 from sklearn.model_selection import KFold, train_test_split
 
+from dpipe.config import register
 from dpipe.dataset import Dataset
 
 
-def get_cv_111(dataset: Dataset, *, val_size, n_splits):
+def extract(l, ids):
+    return [l[i] for i in ids]
+
+
+@register()
+def cv_111(dataset: Dataset, *, val_size, n_splits):
     ids = dataset.patient_ids
     cv = KFold(n_splits=n_splits, shuffle=True, random_state=17)
 
-    train_val_test = []
-    for train, test in cv.split(ids):
-        train = [ids[i] for i in train]
-        test = [ids[i] for i in test]
-        train, val = train_test_split(train, test_size=val_size,
-                                      random_state=25)
-        train_val_test.append((list(train), list(val), list(test)))
+    train_val_test_ids = []
+    for i, (train_val_indices, test_indices) in enumerate(cv.split(ids)):
+        train_val_ids = extract(ids, train_val_indices)
+        test_ids = extract(ids, test_indices)
+        train_ids, val_ids = train_test_split(
+            train_val_ids, test_size=val_size, random_state=25 + i
+        )
+        train_val_test_ids.append((train_ids, val_ids, test_ids))
 
-    return train_val_test
+    return train_val_test_ids
+
+
+def group_train_test_split(x, groups, *, train_size=None, test_size=None,
+                           random_state=None, shuffle=True):
+    train_groups, test_groups = train_test_split(
+        np.unique(groups), train_size=train_size, test_size=test_size,
+        random_state=random_state, shuffle=shuffle
+    )
+    return ([o for i, o in enumerate(x) if groups[i] in train_groups],
+            [o for i, o in enumerate(x) if groups[i] in test_groups])
 
 
 class ShuffleGroupKFold(KFold):
@@ -30,7 +47,8 @@ class ShuffleGroupKFold(KFold):
             yield np.where(train)[0], np.where(test)[0]
 
 
-def get_group_cv_111(dataset: Dataset, *, val_part, n_splits):
+@register()
+def group_cv_111(dataset: Dataset, *, val_size, n_splits):
     """
     In order to use this splitter, your Dataset needs to have
      a 'groups' property.
@@ -39,35 +57,29 @@ def get_group_cv_111(dataset: Dataset, *, val_part, n_splits):
     groups = dataset.groups
     cv = ShuffleGroupKFold(n_splits=n_splits, shuffle=True, random_state=17)
 
-    train_val_test = []
-    for train, test in cv.split(groups):
-        train_groups = [groups[i] for i in train]
-        # in validation and train we also need non-overlapping groups
-        val_groups = int(1 / val_part + .5)
-        spl = ShuffleGroupKFold(n_splits=val_groups, shuffle=True,
-                                random_state=25)
-        train_, val_ = next(spl.split(train_groups))
+    train_val_test_ids = []
+    for i, (train_val_indices, test_indices) in enumerate(cv.split(groups)):
+        train_ids, val_ids = group_train_test_split(
+            extract(ids, train_val_indices), extract(groups, train_val_indices),
+            test_size=val_size, shuffle=True, random_state=25 + i
+        )
+        train_val_test_ids.append((train_ids, val_ids,
+                                   extract(ids, test_indices)))
 
-        train = [ids[i] for i in train_]
-        val = [ids[i] for i in val_]
-        test = [ids[i] for i in test]
-        train_val_test.append((train, val, test))
-
-    return train_val_test
+    return train_val_test_ids
 
 
-def _extract_pure(ids):
-    return list(filter(ids, lambda x: '^' not in x))
-
-
-def get_pure_val_test_group_cv_111(dataset: Dataset, *, val_part, n_splits):
+@register()
+def group_cv_111_pure_011(dataset: Dataset, *, val_size, n_splits):
     """
     In order to use this splitter, your Dataset needs to have
      a 'groups' property.
     """
-    split = get_group_cv_111(dataset=dataset, val_part=val_part,
-                             n_splits=n_splits)
+
+    def _extract_pure(ids):
+        return list(filter(lambda x: '^' not in x, ids))
+
+    split = group_cv_111(dataset=dataset, val_size=val_size, n_splits=n_splits)
 
     return [(train, _extract_pure(val), _extract_pure(test))
             for train, val, test in split]
-
