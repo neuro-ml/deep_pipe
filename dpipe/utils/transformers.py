1ff63b138e168ef7f5dc8b7ac784abafd69e0658
Egor krivov <egor.krivov@frtk.ru>
2017-09-29 12:33:31 +0300

    added transformer to registry system

--- dpipe/utils/transformers.py
+++ dpipe/utils/transformers.py
@@ -3,6 +3,6 @@ import numpy as np
 from dpipe.config import register
 
 
-@register('transform')
+@register(module_type='transform')
 def segm_prob2msegm(x, dataset):
     return dataset.segm2msegm(np.argmax(x, axis=0))
