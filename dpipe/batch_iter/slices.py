3e45a0dc7601f749eedefedc2c64917f06d54b32
Max <maxs987@gmail.com>
2017-10-12 13:13:46 +0300

    added augmentation

--- dpipe/batch_iter/slices.py
+++ dpipe/batch_iter/slices.py
@@ -1,7 +1,10 @@
+import functools
+
 import dpipe.externals.pdp.pdp as pdp
 from dpipe.config import register
 from dpipe.medim.slices import iterate_slices
 from dpipe.medim.utils import load_by_ids
+from dpipe.medim.augmentation import spacial_augmentation
 
 
 @register()
@@ -16,7 +19,28 @@ def slices(ids, load_x, load_y, batch_size, *, shuffle, axis=-1, slices=1,
                     yield x_slice, y_slice
 
     return pdp.Pipeline(
-        pdp.Source(slicer(), buffer_size=30),
+        pdp.Source(slicer(), buffer_size=5),
         pdp.Chunker(chunk_size=batch_size, buffer_size=2),
         pdp.LambdaTransformer(pdp.combine_batches, buffer_size=3)
+    )
+
+
+@register()
+def slices_augmented(ids, load_x, load_y, batch_size, *, shuffle, axis=-1,
+                     slices=1, pad=0, concatenate=None):
+    def slicer():
+        for x, y in load_by_ids(load_x, load_y, ids, shuffle):
+            for x_slice, y_slice in iterate_slices(
+                    x, y, axis=axis, slices=slices, pad=pad,
+                    concatenate=concatenate):
+                if y_slice.any():
+                    yield x_slice, y_slice
+
+    augment = pdp.pack_args(functools.partial(spacial_augmentation, axes=[-1, -2]))
+
+    return pdp.Pipeline(
+        pdp.Source(slicer(), buffer_size=5),
+        pdp.LambdaTransformer(augment, buffer_size=20),
+        pdp.Chunker(chunk_size=batch_size, buffer_size=2),
+        pdp.LambdaTransformer(pdp.combine_batches, buffer_size=3),
     )
