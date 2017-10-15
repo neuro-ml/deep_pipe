a5d85af73e4415b7f733ff1c1b17574e8940ea16
Egor krivov <egor.krivov@frtk.ru>
2017-10-15 15:59:02 +0300

    style change

--- dpipe/model_core/deepmedic_els.py
+++ dpipe/model_core/deepmedic_els.py
@@ -13,10 +13,8 @@ activation = functools.partial(prelu, feature_dims=[1])
 
 def cb(t, n_chans, kernel_size, training, name):
     with tf.variable_scope(name):
-        t = tf.layers.conv3d(t, n_chans, kernel_size, use_bias=False,
-                             data_format='channels_first')
-        t = spatial_batch_norm(t, training=training,
-                               data_format='channels_first')
+        t = tf.layers.conv3d(t, n_chans, kernel_size, use_bias=False, data_format='channels_first')
+        t = spatial_batch_norm(t, training=training, data_format='channels_first')
         return t
 
 
@@ -30,13 +28,11 @@ def build_path(t, blocks, kernel_size, training, name):
         for i, n_chans in enumerate(blocks):
             t = cba(t, n_chans, kernel_size, training, f'cba_{i}_a')
             t = cba(t, n_chans, kernel_size, training, f'cba_{i}_b')
-
         return t
 
 
 downsampling_ops = {
-    'average': lambda x: tf.layers.average_pooling3d(x, 3, 3, 'same',
-                                                     'channels_first'),
+    'average': lambda x: tf.layers.average_pooling3d(x, 3, 3, 'same', 'channels_first'),
     'sampling': lambda x: x[:, :, 1:-1:3, 1:-1:3, 1:-1:3]
 }
 
@@ -44,17 +40,11 @@ downsampling_ops = {
 def build_model(t_det_in, t_con_in, kernel_size, n_classes, training, name, *,
                 path_blocks=(30, 40, 40, 50), n_chans_com=150, dropout):
     with tf.variable_scope(name):
-        t_det = build_path(t_det_in, path_blocks, kernel_size, training,
-                           'detailed')
-
-        t_con = build_path(t_con_in, path_blocks, kernel_size, training,
-                           'context')
-
-        t_con = nearest_neighbour(t_con, 3, data_format='channels_first',
-                                  name='upsample')
+        t_det = build_path(t_det_in, path_blocks, kernel_size, training, 'detailed')
+        t_con = build_path(t_con_in, path_blocks, kernel_size, training, 'context')
+        t_con = nearest_neighbour(t_con, 3, data_format='channels_first', name='upsample')
 
         t_com = tf.concat([t_con, t_det], axis=1)
-
         t_com = cba(dropout(t_com), n_chans_com, 1, training, name='comm_1')
         t_com = cba(dropout(t_com), n_chans_com, 1, training, name='comm_2')
         return cb(t_com, n_classes, 1, training, 'C')
@@ -70,13 +60,11 @@ class DeepMedicEls(ModelCore):
         self.downsampling_op = downsampling_ops[downsampling_type]
 
     def build(self, training_ph):
-        if self.with_dropout:
-            dropout = lambda x: tf.layers.dropout(
-                x, training=training_ph,
-                noise_shape=(tf.shape(x)[0], tf.shape(x)[1], 1, 1, 1),
-            )
-        else:
-            dropout = lambda x: x
+        def dropout(x):
+            if self.with_dropout:
+                return tf.layers.dropout(x, training=training_ph, noise_shape=(tf.shape(x)[0], tf.shape(x)[1], 1, 1, 1))
+            else:
+                return x
 
         input_shape = (None, self.n_chans_in, None, None, None)
         x_det_ph = tf.placeholder(tf.float32, input_shape, name='x_det')
@@ -84,8 +72,7 @@ class DeepMedicEls(ModelCore):
 
         x_con = self.downsampling_op(x_con_ph)
 
-        logits = build_model(x_det_ph, x_con, self.kernel_size,
-                             self.n_chans_out, training_ph, name='deep_medic',
+        logits = build_model(x_det_ph, x_con, self.kernel_size, self.n_chans_out, training_ph, name='deep_medic',
                              dropout=dropout)
 
         return [x_det_ph, x_con_ph], logits
