690aa8dabd5482c48f230de5c38bf36fa9313a82
Max <maxs987@gmail.com>
2017-10-24 19:10:26 +0300

    fixed configs

--- dpipe/config/base.py
+++ dpipe/config/base.py
@@ -11,16 +11,7 @@ EXCLUDED_PATHS = ['externals', 'config', 'medim']
 get_module = functools.partial(get_module, db_path=DB_PATH)
 
 
-def get_resource_manager(config_path, **additional) -> ResourceManager:
+def get_resource_manager(config_path) -> ResourceManager:
     rm = ResourceManager(config_path, get_module=get_module)
-    # Additional arguments:
-    rm.set('config_path', config_path)
-    for key, value in additional.items():
-        rm.set(key, value, override=True)
-
     generate_config(MODULES_FOLDER, DB_PATH, 'dpipe', EXCLUDED_PATHS)
     return rm
-
-
-if __name__ == '__main__':
-    generate_config(MODULES_FOLDER, DB_PATH, 'dpipe', EXCLUDED_PATHS)
--- dpipe/utils/io.py
+++ dpipe/utils/io.py
@@ -1,4 +1,7 @@
+import argparse
 import json
+import re
+
 from dpipe.config import register
 
 
@@ -7,3 +10,28 @@ def load_json(path):
     with open(path, 'r') as f:
         return json.load(f)
 
+
+console_argument = re.compile(r'^--[^\d\W]\w*$')
+
+
+@register('console', 'io')
+class ConsoleArguments:
+    def __init__(self):
+        parser = argparse.ArgumentParser()
+        args = parser.parse_known_args()[1]
+
+        self.args = {}
+        for arg, value in zip(args[::2], args[1::2]):
+            if not console_argument.match(arg):
+                raise ValueError(f'Invalid console argument: {arg}')
+            try:
+                value = int(value)
+            except ValueError:
+                pass
+            self.args[arg] = value
+
+    def __getattr__(self, name):
+        try:
+            return self.args[name]
+        except KeyError:
+            raise AttributeError(f'Console argument {name} not provided') from None
