a911487f4e7103e48453c13f6e15524449743146
Max <maxs987@gmail.com>
2017-10-24 15:57:10 +0300

    scripts refactoring

--- dpipe/batch_iter/pipeline.py
+++ dpipe/batch_iter/pipeline.py
@@ -1,6 +1,6 @@
 import functools
 
-from pdp import Pipeline, One2One, Many2One, Source, pack_args
+from pdp import Pipeline, One2One, Many2One, Source, pack_args, One2Many
 
 from dpipe.config import register_inline, register
 
@@ -9,18 +9,36 @@ register = functools.partial(register, module_type='pdp')
 
 
 @register()
-def pipeline(source, transformers):
+def pipeline(source, transformers, batch_size=None):
     if type(source) is not Source:
         source = Source(source, buffer_size=1)
+    if batch_size is not None:
+        transformers.extend([
+            Many2One(chunk_size=batch_size, buffer_size=2),
+            One2One(pack_args, buffer_size=2),
+        ])
     return Pipeline(source, *transformers)
 
 
 @register()
-def transformer(f, pack=False, **kwargs):
+def source(iterable, buffer_size=1):
+    return Source(iterable, buffer_size=buffer_size)
+
+
+@register()
+def one2one(f, pack=False, n_workers=1, buffer_size=1):
     if pack:
         f = pack_args(f)
-    return One2One(f, **kwargs)
+    return One2One(f, n_workers=n_workers, buffer_size=buffer_size)
 
 
-register_inline(Many2One)
-register_inline(Source)
+@register()
+def one2many(f, pack=False, n_workers=1, buffer_size=1):
+    if pack:
+        f = pack_args(f)
+    return One2Many(f, n_workers=n_workers, buffer_size=buffer_size)
+
+
+@register()
+def many2one(chunk_size, buffer_size=1):
+    return Many2One(chunk_size, buffer_size=buffer_size)
--- dpipe/config/default_parser.py
+++ dpipe/config/default_parser.py
@@ -3,6 +3,30 @@ import argparse
 __all__ = ['get_parser', 'get_args', 'parse_args']
 
 
+class ConsoleArguments:
+    def __init__(self):
+        parser = argparse.ArgumentParser()
+        args = parser.parse_known_args()[1]
+
+        console_argument = re.compile(r'^--[^\d\W]\w*$')
+
+        self.args = {}
+        for arg, value in zip(args[::2], args[1::2]):
+            if not console_argument.match(arg):
+                raise ValueError(f'Invalid console argument: {arg}')
+            try:
+                value = int(value)
+            except ValueError:
+                pass
+            self.args[arg] = value
+
+    def __getattr__(self, name):
+        try:
+            return self.args[name]
+        except KeyError:
+            raise AttributeError(f'Console argument {name} not provided') from None
+
+
 def get_short_name(name: str) -> str:
     """long_informative_name -> lip"""
     return ''.join(map(lambda x: x[0], name.split('_')))
