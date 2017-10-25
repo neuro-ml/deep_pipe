76eb9344f6c68d7869376133962cb132bc7f0652
kondrat1997 <vladimir.kondratenko@phystech.edu>
2017-10-25 15:57:46 +0300

    added weighted bounds dice score, but it isn't optimized

--- dpipe/medim/metrics.py
+++ dpipe/medim/metrics.py
@@ -1,15 +1,17 @@
 import numpy as np
 
-
 def soft_weighted_dice_score(a, b, empty_val: float = 0):
     """
     Realization of dice metric proposed in https://arxiv.org/pdf/1707.01992.pdf
 
     Parameters
     ----------
-    a - predicted probability maps
-    b - ground truth
-    empty_val - default value to avoid division by zero
+    a: ndarray
+        Predicted probability maps
+    b: ndarray of np.bool
+        Ground truth
+    empty_val: int
+        Default value to avoid division by zero
     """
     assert b.dtype == np.bool
     assert a.shape == b.shape
@@ -41,6 +43,73 @@ def dice_score(x, y, empty_val=0):
 def multichannel_dice_score(a, b, empty_val=0):
     dices = [dice_score(x, y, empty_val=empty_val) for x, y in zip(a, b)]
     return dices
+
+def find_bounds(y):
+    """
+
+    Parameters
+    ----------
+    y: ndarray of np.bool
+        Multichannel binary mask
+
+    """
+    assert y.dtype == np.bool
+
+    all_bounds = []
+    for channel in y:
+        bounds = np.zeros(channel.shape).astype(np.bool)
+        for i in range(len(channel.shape)):
+            temp_bound = (channel & (~np.roll(channel, 1, axis=i))) | (channel & (~np.roll(channel, -1, axis=i)))
+            bounds = bounds | temp_bound
+
+        all_bounds.append(bounds)
+    return np.array(all_bounds)
+
+def weighted_bounds_dice_score(a,b, size_of_bound = 1, empty_val = 0):
+    """
+
+    Parameters
+    ----------
+    a: ndarray of np.bool
+        Multichannel binary mask
+    b: ndarray
+        Predicted probability maps
+    size_of_bound: int
+    empty_val: int
+        Default value to avoid division by zero
+    Returns
+    -------
+
+    """
+
+    assert b.dtype == np.bool
+    assert a.shape == b.shape
+
+    temp_b = b.copy()
+    bounds = []
+
+    for i in range(size_of_bound):
+        bounds.append(find_bounds(temp_b))
+        temp_b = ~np.logical_xor(temp_b, bounds[-1])
+
+    bounds = np.array(bounds).astype(np.int) * np.arange(1,size_of_bound+1)
+    temp_b = np.array(temp_b).astype(np.int)
+
+    for bound in bounds:
+        temp_b = temp_b + bound
+    #now temp_b is mask with weighted boundes
+
+    wbds = 0
+    num_classes = a.shape[0]
+
+    for x, y in zip(a, temp_b):
+        num = 2 * np.sum(x * y)
+        den = np.sum(y) + np.sum(x)
+
+        wbds += empty_val if den == 0 else num / den
+
+    wbds = wbds / num_classes
+    return wbds
 
 
 # Before using this module, install the dependencies:
