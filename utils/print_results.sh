818e53ec7622e76def547a54de292634a30b5fbc
Egor krivov <egor.krivov@frtk.ru>
2018-02-03 17:33:52 +0300

    print results methods from utils were updated for new snakemake files and output files

--- utils/print_results.sh
+++ utils/print_results.sh
@@ -1,11 +1,14 @@
 # Simple script to print metrics across experiments
-# Example ./print_results mean ~/exp/brats_deepmedic/ test_dices
-
-set -e
+# Example ./print_results mean test_metrics/dice.json ~/exp/brats_deepmedic/
 
 UTILS_PATH=$(dirname $0)
 
-RESULT_FILES=$(find $2/experiment_* -maxdepth 1 -name $3.json)
+RESULT_FILES=$(ls $3/experiment_*/$2 2>/dev/null)
+
+if [ $? -ne 0 ]; then
+    echo "No results found"
+    exit
+fi
 
 echo $(echo ${RESULT_FILES} | wc -w) experiment resuts found
 ${UTILS_PATH}/jq -s add ${RESULT_FILES} | python ${UTILS_PATH}/aggregate.py $1
