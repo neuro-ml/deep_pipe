2c1a83303241e498f4acc4ed2fa0a36beb975dd3
Egor krivov <egor.krivov@frtk.ru>
2018-02-07 20:48:56 +0300

    more reasonable arguments for print results script

--- utils/print_results.sh
+++ utils/print_results.sh
@@ -3,7 +3,7 @@
 
 UTILS_PATH=$(dirname $0)
 
-RESULT_FILES=$(ls $3/experiment_*/$2 2>/dev/null)
+RESULT_FILES=$(ls $3/experiment_*/$1 2>/dev/null)
 
 if [ $? -ne 0 ]; then
     echo "No results found"
@@ -11,4 +11,4 @@ if [ $? -ne 0 ]; then
 fi
 
 echo $(echo ${RESULT_FILES} | wc -w) experiment resuts found
-${UTILS_PATH}/jq -s add ${RESULT_FILES} | python ${UTILS_PATH}/aggregate.py $1
+${UTILS_PATH}/jq -s add ${RESULT_FILES} | python ${UTILS_PATH}/aggregate.py $2
