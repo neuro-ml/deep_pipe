8e7d78898fb19819d188957437b6b2a608d5c1e8
Boris <Dustpelt@mgmt.cobrain.stech>
2018-03-29 14:55:53 +0300

    fixed focal loss

--- dpipe/torch/utils.py
+++ dpipe/torch/utils.py
@@ -75,35 +75,43 @@ class LinearFocalLoss(_Loss):
         ) / self.gamma
 
 
-def focal_weight(probs, target, gamma):
-    weight = (1 - 2 * probs) * target + probs
-    return weight ** gamma
+def focal_loss_with_logits(logits, target, gamma=2, weight=None, size_average=True, reduce=True):
+    """Function that measures Focal Loss between target and output
+    logits.
 
+    Args:
+        logits: Variable of arbitrary shape
+        target: Variable of the same shape as input
+        gamma (float): The power of focal loss factor
+        weight (Variable, optional): a manual rescaling weight
+                if provided it's repeated to match input tensor shape
+        size_average (bool, optional): By default, the losses are averaged
+                over observations for each minibatch. However, if the field
+                :attr:`size_average` is set to ``False``, the losses are instead summed
+                for each minibatch. Default: ``True``
+        reduce (bool, optional): By default, the losses are averaged or summed over
+                observations for each minibatch depending on :attr:`size_average`. When :attr:`reduce`
+                is ``False``, returns a loss per logits/target element instead and ignores
+                :attr:`size_average`. Default: ``True``
+    """
+    if not (target.size() == logits.size()):
+        raise ValueError("Target size ({}) must be the same as logits size ({})".format(target.size(), logits.size()))
 
-# TODO: unify
-class FocalLossWithLogits(_Loss):
-    def __init__(self, gamma, size_average=True):
-        super().__init__(size_average)
-        self.gamma = gamma
+    min_val = - logits.clamp(min=0)
+    max_val = (-logits).clamp(min=0)
+    
+    prob = (min_val + logits).exp() / (min_val.exp() + (min_val + logits).exp())
+    
+    loss = ( (1 - 2*prob)*target + prob ) ** gamma \
+    * ( logits - logits * target + max_val + ((-max_val).exp() + (-logits - max_val).exp()).log() )
+    # * F.binary_cross_entropy_with_logits(logits, target, reduce=False)
 
-    def forward(self, logits, target):
-        bce = functional.binary_cross_entropy_with_logits(logits, target, size_average=False)
-        loss = focal_weight(torch.sigmoid(logits), target, self.gamma) * bce
+    if weight is not None:
+        loss = loss * weight
 
-        if self.size_average:
-            return loss.mean()
-        return loss.sum()
-
-
-class FocalLoss(_Loss):
-    def __init__(self, gamma, size_average=True):
-        super().__init__(size_average)
-        self.gamma = gamma
-
-    def forward(self, input, target):
-        bce = functional.binary_cross_entropy(input, target, size_average=False)
-        loss = focal_weight(input, target, self.gamma) * bce
-
-        if self.size_average:
-            return loss.mean()
+    if not reduce:
+        return loss
+    elif size_average:
+        return loss.mean()
+    else:
         return loss.sum()
