c5e74988af294764efba087074ab250dea054175
Max <maxs987@gmail.com>
2018-08-29 13:11:04 +0300

    generalized `make_infinite_batch_iter`

--- dpipe/batch_iter/sources.py
+++ dpipe/batch_iter/sources.py
@@ -1,9 +1,20 @@
+from typing import Sequence, Callable
+
 import numpy as np
 
 from dpipe.medim.utils import pam
 
 
-def sample_ids(ids, weights=None):
+def sample_ids(ids: Sequence, weights: Sequence[float] = None):
+    """
+    Infinitely yield identifiers randomly sampled from `ids` according to `weights`.
+
+    Parameters
+    ----------
+    ids: Sequence
+    weights: Sequence[float]
+        The weights associated with each id. If None, the weights are assumed to be equal.
+    """
     if weights is not None:
         weights = np.asarray(weights)
         assert (weights >= 0).all() and (weights > 0).any(), weights
@@ -13,6 +24,16 @@ def sample_ids(ids, weights=None):
         yield np.random.choice(ids, p=weights)
 
 
-def load_by_random_id(*loaders, ids, weights=None):
+def load_by_random_id(*loaders: Callable, ids: Sequence, weights: Sequence[float] = None):
+    """
+    Infinitely yield objects loaded by identifiers randomly sampled from `ids` according to `weights`.
+
+    Parameters
+    ----------
+    loaders: Callable(identifier)
+    ids: Sequence
+    weights: Sequence[float]
+        The weights associated with each id. If None, the weights are assumed to be equal.
+    """
     for id_ in sample_ids(ids, weights):
         yield tuple(pam(loaders, id_))
