99a6f46e3dbe87d5c3b11787d0b709c816db54f7
Max <maxs987@gmail.com>
2018-10-31 22:24:40 +0300

    generalized map_ids_to_disk with the  function

--- dpipe/commands.py
+++ dpipe/commands.py
@@ -24,15 +24,16 @@ def transform(input_path, output_path, transform_fn):
 
 
 def load_from_folder(path: str):
-    """Yields (id, object) pairs loaded from `path`."""
+    """Yields (id, object) pairs loaded from ``path``."""
     # TODO: generalize with a loader
     for filename in sorted(os.listdir(path)):
         yield np_filename2id(filename), np.load(os.path.join(path, filename))
 
 
-def map_ids_to_disk(func: Callable[[str], object], ids: Iterable[str], output_path: str, exist_ok: bool = False):
+def map_ids_to_disk(func: Callable[[str], object], ids: Iterable[str], output_path: str,
+                    exist_ok: bool = False, save: Callable = np.save):
     """
-    Apply `func` to each id from `ids` and save each output to `output_path`.
+    Apply ``func`` to each id from ``ids`` and save each output to ``output_path`` using ``save``.
     If `exists_ok` is True the existing files will be ignored, otherwise an exception is raised.
     """
     os.makedirs(output_path, exist_ok=exist_ok)
@@ -48,13 +49,13 @@ def map_ids_to_disk(func: Callable[[str], object], ids: Iterable[str], output_pa
         if isinstance(value, np.ndarray) and np.issubdtype(value.dtype, np.floating):
             value = value.astype(np.float16)
 
-        np.save(output, value)
+        save(output, value)
         # saving some memory
         del value
 
 
-def predict(ids, output_path, load_x, predict_fn, exist_ok=False):
-    map_ids_to_disk(lambda identifier: predict_fn(load_x(identifier)), tqdm(ids), output_path, exist_ok)
+def predict(ids, output_path, load_x, predict_fn, exist_ok=False, save=np.save):
+    map_ids_to_disk(lambda identifier: predict_fn(load_x(identifier)), tqdm(ids), output_path, exist_ok, save)
 
 
 def evaluate_aggregated_metrics(load_y_true, metrics: dict, predictions_path, results_path, exist_ok=False):
