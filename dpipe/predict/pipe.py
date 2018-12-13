4b8ab139c8a31d18377dc6f3d465aac59b2671d6
Max <maxs987@gmail.com>
2018-12-14 00:15:39 +0300

    refactored predicts to match the new interface

--- dpipe/predict/pipe.py
+++ dpipe/predict/pipe.py
@@ -1,7 +1,7 @@
-"""Functions that build prediction pipeline. Each prepares input in certain way, predicts and then fixes output.
-So nearly all functions receive `predict` argument."""
-from functools import partial
-
+"""
+Functions that build prediction pipeline.
+Each prepares input in certain way, predicts and then fixes output.
+"""
 import numpy as np
 
 from dpipe.medim.utils import extract_dims, pad
@@ -12,21 +12,27 @@ from .functional import trim_spatial_size, pad_to_dividable
 from .utils import add_dims
 
 
-def add_remove_first_dims(*inputs, predict, ndim=1):
+# TODO: add a better description and details about predicts' interface
+
+def add_remove_first_dims(ndim=1, *, predict):
     """Adds `ndim` first dimensions to `inputs`, predicts with `predict` and removes added dimensions.
     Useful to add batch dimension for neural networks."""
-    return extract_dims(predict(*add_dims(*inputs, ndims=ndim)), ndim=ndim)
+    return lambda *inputs: extract_dims(predict(*add_dims(*inputs, ndims=ndim)), ndim=ndim)
 
 
-def pad_trim_last_dims_to_dividable(x, predict, divisor, ndim=3):
+def pad_trim_last_dims_to_dividable(divisor, ndim=3, *, predict):
     """Pads `x`'s last `ndim` dimensions to be dividable by ``divisor`, predicts with `predict` and then trims spatial
     dimensions to have the same shape as in `x`."""
-    x_padded = pad_to_dividable(x, divisor, ndim=ndim)
-    y = predict(x_padded)
-    return trim_spatial_size(y, spatial_size=np.array(x.shape[-ndim:]))
+
+    def predictor(x):
+        x_padded = pad_to_dividable(x, divisor, ndim=ndim)
+        y = predict(x_padded)
+        return trim_spatial_size(y, spatial_size=np.array(x.shape[-ndim:]))
+
+    return predictor
 
 
-def divide_combine_patches(x, predict, patch_size: np.ndarray, stride: np.ndarray):
+def divide_combine_patches(patch_size: np.ndarray, stride: np.ndarray, predict):
     """Split `x` into patches, usually because network can't process whole image because of the memory limitations.
     Network is assumed to return `y_patch_size`, given that `x_patch_size` input is provided.
     So, if `x_patch_size` is more than `y_patch_size`, network is assumed to consume
@@ -39,15 +45,23 @@ def divide_combine_patches(x, predict, patch_size: np.ndarray, stride: np.ndarra
     check_len(patch_size, stride)
     ndim = len(patch_size)
 
-    padding = np.array((x.ndim - ndim) * [0] + list((patch_size - stride) // 2))[None][[0, 0]].T
-    x_padded = pad(x, padding=padding, padding_values=np.min(x, axis=ndim2spatial_axes(ndim), keepdims=True))
-    x_parts = grid.divide(x_padded, patch_size=patch_size, stride=stride, valid=False)
-    return grid.combine(predict(x_parts), x.shape[-ndim:])
+    def predictor(x):
+        padding = np.array((x.ndim - ndim) * [0] + list((patch_size - stride) // 2))[None][[0, 0]].T
+        x_padded = pad(x, padding=padding, padding_values=np.min(x, axis=ndim2spatial_axes(ndim), keepdims=True))
+        x_parts = grid.divide(x_padded, patch_size=patch_size, stride=stride, valid=False)
+        return grid.combine(predict(x_parts), x.shape[-ndim:])
+
+    return predictor
 
 
+# TODO: test this
 def chain_predicts(*predicts):
     """Chain functions from this module together."""
-    chain = predicts[-1]
-    for predict in reversed(predicts[:-1]):
-        chain = partial(predict, predict=chain)
-    return chain
+
+    def make_predictor(predict):
+        chain = predict
+        for predict_ in reversed(predicts):
+            chain = predict_(predict=chain)
+        return chain
+
+    return make_predictor
