f1ae2597c44ab08da43291b117b5756769ee9186
Max <maxs987@gmail.com>
2020-03-09 23:28:10 +0300

    legacy builder

--- dpipe/medim/io.py
+++ dpipe/medim/io.py
@@ -3,3 +3,4 @@ from warnings import warn
 from dpipe.io import *
 
 warn('`dpipe.medim.io` has been moved to `dpipe.io`.', UserWarning, stacklevel=2)
+warn('`dpipe.medim.io` has been moved to `dpipe.io`.', DeprecationWarning, stacklevel=2)
