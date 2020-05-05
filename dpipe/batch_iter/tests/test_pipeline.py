0f95759fbe47e07b7f90a869f7253e32bede0b92
Max <maxs987@gmail.com>
2020-05-05 17:10:47 +0300

    adaptive `batch_size`

--- dpipe/batch_iter/tests/test_pipeline.py
+++ dpipe/batch_iter/tests/test_pipeline.py
@@ -1,39 +1,63 @@
-import unittest
 from itertools import repeat
 
-from dpipe.batch_iter.pipeline import Infinite
+import pytest
+
+from dpipe.batch_iter import Infinite, combine_to_arrays
 
 
-class TestSimple(unittest.TestCase):
-    @staticmethod
-    def pipeline(source, iters=1, transformers=()):
-        return Infinite(source, *transformers, batch_size=1, batches_per_epoch=iters, buffer_size=1)
+def pipeline(source, iters=1, transformers=(), batch_size=1, combiner=combine_to_arrays):
+    return Infinite(source, *transformers, batch_size=batch_size, batches_per_epoch=iters, buffer_size=1,
+                    combiner=combiner)
 
-    def test_elements(self):
-        p = self.pipeline(repeat([1, 2]), 1)
-        batch = list(p())
-        self.assertTupleEqual(((1,), (2,)), batch[0])
 
-    def test_iters(self):
-        for i in [1, 5, 10, 100, 450]:
-            p = self.pipeline(repeat([1]), i)
-            self.assertEqual(len(list(p())), i)
+def test_elements():
+    p = pipeline(repeat([1, 2]), 1)
+    batch = list(p())
+    assert ((1,), (2,)) == batch[0]
 
-    def test_enter(self):
-        p = self.pipeline(repeat([1]), 1)
-        with p:
-            self.assertEqual(len(list(p())), 1)
 
-    def test_multiple_iter(self):
-        p = self.pipeline(repeat([1]), 1)
+def test_batch_size():
+    for bs in [1, 5, 10]:
+        p = pipeline(repeat([1]), batch_size=bs)
+        assert len(list(p())[0][0]) == bs
 
-        self.assertEqual(len(list(p())), 1)
-        self.assertEqual(len(list(p())), 1)
-        self.assertEqual(len(list(p())), 1)
+    with pytest.raises(TypeError):
+        pipeline([1], batch_size=0.5)
 
-        p.close()
+    with pytest.raises(TypeError):
+        pipeline([1], batch_size=[1])
 
-    def test_del(self):
-        p = self.pipeline(repeat([1]), 1)
-        self.assertEqual(len(list(p())), 1)
-        del p
+    def max_three(chunk, item):
+        return sum(map(len, chunk + [item])) <= 3
+
+    with pipeline([[1, 2], [1], [1], [1], [1], [1, 2, 3], [1, 2], [1]], 4, batch_size=max_three, combiner=list) as p:
+        for b in p():
+            assert sum(map(len, b)) == 3
+
+
+def test_iters():
+    for i in [1, 5, 10, 100, 450]:
+        p = pipeline(repeat([1]), i)
+        assert len(list(p())) == i
+
+
+def test_enter():
+    p = pipeline(repeat([1]), 1)
+    with p as pp:
+        assert len(list(pp())) == 1
+
+
+def test_multiple_iter():
+    p = pipeline(repeat([1]), 1)
+
+    assert len(list(p())) == 1
+    assert len(list(p())) == 1
+    assert len(list(p())) == 1
+
+    p.close()
+
+
+def test_del():
+    p = pipeline(repeat([1]), 1)
+    assert len(list(p())) == 1
+    del p
