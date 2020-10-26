77604bf13c64f1b18edba100533fb1c7e1119346
Vladislav Proskurov <rilshok@ya.ru>
2020-10-26 15:09:13 +0700

    Accelerated sampling function and its testing in comparison with numpy

--- dpipe/batch_iter/sources.py
+++ dpipe/batch_iter/sources.py
@@ -2,7 +2,10 @@ from typing import Sequence, Callable, Union
 
 import numpy as np
 
+from bisect import bisect
+
 from dpipe.itertools import pam, squeeze_first
+
 
 __all__ = 'sample', 'load_by_random_id'
 
@@ -10,7 +13,7 @@ __all__ = 'sample', 'load_by_random_id'
 def sample(sequence: Sequence, weights: Sequence[float] = None, random_state: Union[np.random.RandomState, int] = None):
     """
     Infinitely yield samples from ``sequence`` according to ``weights``.
-
+    
     Parameters
     ----------
     sequence: Sequence
@@ -20,18 +23,24 @@ def sample(sequence: Sequence, weights: Sequence[float] = None, random_state: Un
         Should be the same size as ``sequence``.
     random_state
         if not None - used to set the random seed for reproducibility reasons.
-    """
-    if weights is not None:
+    """    
+    if not isinstance(random_state, np.random.RandomState):
+        random_state = np.random.RandomState(random_state)
+        
+    if weights is None:
+        # works faster than random_state.randint(0, len(sequence))
+        get_index = lambda: int(random_state.rand()*(len(sequence)))
+    else:
+        assert(len(sequence) == len(weights)), 'len(sequence) is not equal to len(weights)'
         weights = np.asarray(weights)
         assert (weights >= 0).all() and (weights > 0).any(), weights
         weights = weights / weights.sum()
-
-    if not isinstance(random_state, np.random.RandomState):
-        random_state = np.random.RandomState(random_state)
-
+        weights_sort_args  = np.argsort(weights)
+        weights_accum_sort = np.add.accumulate(weights[weights_sort_args])
+        get_index = lambda: weights_sort_args[bisect(weights_accum_sort,random_state.rand())]        
+        
     while True:
-        index = random_state.choice(len(sequence), p=weights)
-        yield sequence[index]
+        yield sequence[get_index()]
 
 
 def load_by_random_id(*loaders: Callable, ids: Sequence, weights: Sequence[float] = None,
@@ -39,7 +48,7 @@ def load_by_random_id(*loaders: Callable, ids: Sequence, weights: Sequence[float
     """
     Infinitely yield objects loaded by ``loaders`` according to the identifier from ``ids``.
     The identifiers are randomly sampled from ``ids`` according to the ``weights``.
-
+    
     Parameters
     ----------
     loaders: Callable
