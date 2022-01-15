e6719cd21420550fd80e3a821d4ee5e344ab4e84
Vladislav Proskurov <rilshok@pm.me>
2022-01-15 21:27:14 +0300

    which fold

--- dpipe/train/logging.py
+++ dpipe/train/logging.py
@@ -164,7 +164,20 @@ class WANDBLogger(Logger):
         )
 
         current_fold_root = Path(self._experiment.dir).parent.parent.parent.parent
+        current_experiment_number = int(current_fold_root.name.replace('experiment_', ''))
         experiment_root = current_fold_root.parent
+
+        # find out if the experiment is cut into several folds
+        cut_into_folds = (
+            len(
+                [
+                    p
+                    for p in experiment_root.glob("*")
+                    if p.name.startswith("experiment_") and p.is_dir()
+                ]
+            )
+            > 1
+        )
 
         if run_name is not None:
             self._experiment.name = run_name  # can be changed manually
