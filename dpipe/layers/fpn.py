3aba379902bb4bf8e6c82b1d5ac518b576a97ab1
Anihilatorgunn <n.ushakov@ira-labs.com>
2025-12-03 15:14:45 +0300

    check_shape_equal is kwonly

--- dpipe/layers/fpn.py
+++ dpipe/layers/fpn.py
@@ -130,7 +130,7 @@ def interpolate_merge(merge: Callable, order: int = 0):
     return lambda left, down: merge(*interpolate_to_left(left, down, order))
 
 
-def interpolate_to_left(left: torch.Tensor, down: torch.Tensor, order: int = 0, check_shape_equal: bool = False):
+def interpolate_to_left(left: torch.Tensor, down: torch.Tensor, order: int = 0, *, check_shape_equal: bool = False):
     if check_shape_equal and np.equal(left.shape, down.shape).all():
         message = 'interpolate_to_left is called with  =True. This may lead to branching.'
         warn(message, UserWarning)
